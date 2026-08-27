d076021b88da50d44418889d625ff57011780f60
Martin Cernak <martin@macbook>
2026-08-27 22:07:35 +0200

    Add single thread cpu bound test

---
+++ BenchItOut/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Security.Cryptography;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Running;
+
+namespace BenchItOut
+{
+    [MarkdownExporter]
+    public class SingleThreadCpuBound
+    {
+        private readonly SHA256 _sha256 = SHA256.Create();
+        private readonly MD5 _md5 = MD5.Create();
+        private byte[]? _data;
+
+        [Params(1000, 10000)]
+        public int N;
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            _data = new byte[N];
+            new Random(42).NextBytes(_data);
+        }
+
+        [Benchmark]
+        public byte[] Sha256() => _sha256.ComputeHash(_data!);
+
+        [Benchmark]
+        public byte[] Md5() => _md5.ComputeHash(_data!);
+    }
+    
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<SingleThreadCpuBound>();
+        }
+    }
+}
