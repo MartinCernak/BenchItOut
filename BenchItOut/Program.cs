9072ede0bb7c71dea3693467ab906f8e38be942b
Martin Cernak <martin@macbook>
2026-09-08 21:25:24 +0200

    Use ConsoleAppFramework to add arguments

--- BenchItOut/Program.cs
+++ BenchItOut/Program.cs
@@ -1,6 +1,17 @@
 ﻿using BenchItOut;
 using BenchmarkDotNet.Running;
+using ConsoleAppFramework;
 
-BenchmarkRunner.Run<SingleThreadCrypto>();
-BenchmarkRunner.Run<SingleThreadCpuBound>();
-BenchmarkRunner.Run<MultiThreadCpuBound>();
+var app = ConsoleApp.Create();
+
+app.Add("single-thread-crypto", () => BenchmarkRunner.Run<SingleThreadCrypto>());
+app.Add("single-thread-cpu-bound", () => BenchmarkRunner.Run<SingleThreadCpuBound>());
+app.Add("multi-thread-cpu-bound", () => BenchmarkRunner.Run<MultiThreadCpuBound>());
+app.Add("", () =>
+{
+    BenchmarkRunner.Run<SingleThreadCrypto>();
+    BenchmarkRunner.Run<SingleThreadCpuBound>();
+    BenchmarkRunner.Run<MultiThreadCpuBound>();
+});
+
+app.Run(args);
